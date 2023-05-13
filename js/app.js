261a280d98d7fda18d7cad7700fa408ca6d6b343
unknown <Mohammad-Abdelhadi>
2023-05-13 04:34:16 +0300

    done the left side

--- js/app.js
+++ js/app.js
@@ -64,3 +64,38 @@
 //   .catch((error) => {
 //     console.error("Error:", error);
 //   });
+
+// my code _______________
+const accessToken = "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";
+const username = "Mohammad-Abdelhadi";
+// const username = "Karim-a-khaled";
+
+fetch(`https://api.github.com/users/${username}`, {
+  //   headers: {
+  //     Authorization: `token ${accessToken}`,
+  //   },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    const leftside_info = document.querySelector(".leftside-info");
+    const picture = leftside_info.querySelector("img");
+    const userinfo = document.querySelector(".userinfo");
+    const bio = userinfo.querySelector(".bio");
+    const location = userinfo.querySelector(".location");
+
+    bio.textContent = data.bio || "There's No Bio!";
+    location.textContent = data.location || "There's No Location";
+    picture.src = data.avatar_url;
+
+    const username = document.querySelector(".nameofuser .username");
+    username.textContent = data.name;
+
+    const secondname = document.querySelector(".nameofuser .secondname");
+    secondname.textContent = data.login;
+    nameofuser.appendChild(username);
+    userinfo.appendChild(bio);
+    userinfo.appendChild(location);
+});
+
+    // Append the modified elements
+
