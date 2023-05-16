29d3499f3e499c6cc80e7594ab305ac9148b371a
unknown <Mohammad-Abdelhadi>
2023-05-16 10:11:19 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
-
-
+// const username = "Mohammad-Abdelhadi";
 const username = "Mohammad-Abdelhadi";
 // const username = "Karim-a-khaled";
+// const username = "baselmq";
+
+// username = "majdJadallah ";
+// username = "shahedalazzam ";
+// username = "AyhamZaid ";
+let token = "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";
 
 fetch(`https://api.github.com/users/${username}`, {
-  //   headers: {
-  //     Authorization: `token ${accessToken}`,
-  //   },
+  headers: { Authorization: `token ${token}` },
 })
   .then((response) => response.json())
   .then((data) => {
@@ -16,19 +19,44 @@ fetch(`https://api.github.com/users/${username}`, {
     const bio = userinfo.querySelector(".bio");
     const location = userinfo.querySelector(".location");
 
-    bio.textContent = data.bio || "There's No Bio!";
-    location.textContent = data.location || "There's No Location";
+    bio.textContent = data.bio || "";
+    location.textContent = data.location || "";
     picture.src = data.avatar_url;
 
-    const username = document.querySelector(".nameofuser .username");
-    username.textContent = data.name;
+    const usernameElement = document.querySelector(".nameofuser .username");
+    usernameElement.textContent = data.name;
 
     const secondname = document.querySelector(".nameofuser .secondname");
     secondname.textContent = data.login;
-    nameofuser.appendChild(username);
     userinfo.appendChild(bio);
     userinfo.appendChild(location);
-});
+    return fetch(data.repos_url);
+  })
+  .then((response) => response.json())
+  .then((repositories) => {
+    const allrepoinfo = document.querySelector(".container-repos"); // Assuming there's only one element with the class "all-repo-info"
+    for (let i = 0; i < repositories.length; i++) {
+      const reposubject = document.createElement("div");
+      const repoLink = document.createElement("a");
+      repoLink.href = ""; // Set the repository URL here
+      repoLink.textContent = repositories[i].name;
 
-    // Append the modified elements
+      const publicSpan = document.createElement("span");
+      publicSpan.className = "public";
+      publicSpan.textContent = "Public";
 
+      const topOfRepo = document.createElement("div");
+      topOfRepo.className = "top-of-repo";
+      topOfRepo.appendChild(repoLink);
+      topOfRepo.appendChild(publicSpan);
+
+      const botOfRepo = document.createElement("p");
+      botOfRepo.className = "bot-of-repo";
+      botOfRepo.textContent = repositories[i].language; // Assuming you want to display the repository's language
+
+      reposubject.appendChild(topOfRepo);
+      reposubject.appendChild(botOfRepo);
+
+      allrepoinfo.appendChild(reposubject);
+    }
+  });
