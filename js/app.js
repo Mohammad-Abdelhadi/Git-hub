4169f2b78cc625ab91651cd88a4419656e3d9c29
unknown <Mohammad-Abdelhadi>
2023-05-16 11:38:56 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,62 +1,70 @@
-// const username = "Mohammad-Abdelhadi";
-const username = "Mohammad-Abdelhadi";
-// const username = "Karim-a-khaled";
-// const username = "baselmq";
+const username = "mohammad-abdelhadi";
+// let token = "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";
+let divsearch = document.querySelector(".search");
+let input = divsearch.querySelector(".input");
+let button = divsearch.querySelector(".button");
+button.addEventListener("click", function () {
 
-// username = "majdJadallah ";
-// username = "shahedalazzam ";
-// username = "AyhamZaid ";
-let token = "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";
-
-fetch(`https://api.github.com/users/${username}`, {
-  headers: { Authorization: `token ${token}` },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    const leftside_info = document.querySelector(".leftside-info");
-    const picture = leftside_info.querySelector("img");
-    const userinfo = document.querySelector(".userinfo");
-    const bio = userinfo.querySelector(".bio");
-    const location = userinfo.querySelector(".location");
-
-    bio.textContent = data.bio || "";
-    location.textContent = data.location || "";
-    picture.src = data.avatar_url;
-
-    const usernameElement = document.querySelector(".nameofuser .username");
-    usernameElement.textContent = data.name;
-
-    const secondname = document.querySelector(".nameofuser .secondname");
-    secondname.textContent = data.login;
-    userinfo.appendChild(bio);
-    userinfo.appendChild(location);
-    return fetch(data.repos_url);
+  fetch(`https://api.github.com/users/${input.value}`, {
+    // headers: { Authorization: `token ${token}` },
   })
-  .then((response) => response.json())
-  .then((repositories) => {
-    const allrepoinfo = document.querySelector(".container-repos"); // Assuming there's only one element with the class "all-repo-info"
-    for (let i = 0; i < repositories.length; i++) {
-      const reposubject = document.createElement("div");
-      const repoLink = document.createElement("a");
-      repoLink.href = ""; // Set the repository URL here
-      repoLink.textContent = repositories[i].name;
+    .then((response) => response.json())
+    .then((data) => {
+      const leftside_info = document.querySelector(".leftside-info");
+      const picture = leftside_info.querySelector("img");
+      const userinfo = document.querySelector(".userinfo ");
+      const bio = userinfo.querySelector(".bio");
+      const location = userinfo.querySelector(".location");
 
-      const publicSpan = document.createElement("span");
-      publicSpan.className = "public";
-      publicSpan.textContent = "Public";
+      bio.textContent = data.bio || "";
+      location.textContent = data.location || " ";
+      picture.src = data.avatar_url || " theres not photo";
 
-      const topOfRepo = document.createElement("div");
-      topOfRepo.className = "top-of-repo";
-      topOfRepo.appendChild(repoLink);
-      topOfRepo.appendChild(publicSpan);
+      const usernameElement = document.querySelector(".nameofuser .username");
+      usernameElement.textContent = data.name;
 
-      const botOfRepo = document.createElement("p");
-      botOfRepo.className = "bot-of-repo";
-      botOfRepo.textContent = repositories[i].language; // Assuming you want to display the repository's language
+      const secondname = document.querySelector(".nameofuser .secondname");
+      secondname.textContent = data.login;
+      userinfo.appendChild(usernameElement);
+      userinfo.appendChild(secondname);
+      userinfo.appendChild(bio);
+      userinfo.appendChild(location);
+    });
+  fetch(`https://api.github.com/users/${input.value}/repos`)
+    .then((response) => response.json())
+    .then((repositories) => {
+      const allrepoinfo = document.querySelector(".container-repos"); // Assuming there's only one element with the class "container-repos"
+      for (let i = 0; i < repositories.length; i++) {
+        const reposubject = document.createElement("div");
+        reposubject.className = "all-repo-info";
+        reposubject.style.border = "2px solid #d0d7de";
+        reposubject.style.borderRadius = "16px";
+        const repoLink = document.createElement("a");
+        repoLink.href = repositories[i].html_url; // Set the repository URL here
+        repoLink.textContent = repositories[i].name;
+        repoLink.setAttribute("target", "_blank");
 
-      reposubject.appendChild(topOfRepo);
-      reposubject.appendChild(botOfRepo);
+        const publicSpan = document.createElement("span");
+        publicSpan.className = "public";
+        publicSpan.textContent = "Public";
 
-      allrepoinfo.appendChild(reposubject);
-    }
-  });
+        const topOfRepo = document.createElement("div");
+        topOfRepo.className = "top-of-repo";
+        topOfRepo.appendChild(repoLink);
+        topOfRepo.appendChild(publicSpan);
+
+        const botOfRepo = document.createElement("p");
+        botOfRepo.className = "bot-of-repo";
+        botOfRepo.textContent =
+          repositories[i].language || "There's No Specific language"; // Assuming you want to display the repository's language
+
+        reposubject.appendChild(topOfRepo);
+        reposubject.appendChild(botOfRepo);
+
+        allrepoinfo.appendChild(reposubject);
+
+      }
+
+    });
+
+});
