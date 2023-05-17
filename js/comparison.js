61ce1b52c572e3f46b0cc1f11ac5257c0155fbe9
unknown <Mohammad-Abdelhadi>
2023-05-17 12:25:33 +0300

    done

--- js/comparison.js
+++ js/comparison.js
@@ -3,8 +3,9 @@ let username = document.querySelector(".username");
 let form = document.querySelector(".form");
 let reponumber = document.querySelector(".reponumber");
 let img = document.querySelector(".leftside img");
+
 form.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent form submission
+  event.preventDefault();
 
   fetch(`https://api.github.com/users/${input.value}`)
     .then((response) => response.json())
@@ -12,10 +13,11 @@ form.addEventListener("submit", function (event) {
       username.innerHTML = data.name || data.login;
       img.src = data.avatar_url;
     });
+
   fetch(`https://api.github.com/users/${input.value}/repos`)
     .then((response) => response.json())
     .then((repositories) => {
-      reponumber.innerHTML = `Number Of Repos : ${repositories.length}`;
+      reponumber.innerHTML = `Number Of Repos: ${repositories.length}`;
     });
 });
 
@@ -34,9 +36,23 @@ formtwo.addEventListener("submit", function (event) {
       usernametwo.innerHTML = data.name || data.login;
       imgtwo.src = data.avatar_url;
     });
+
   fetch(`https://api.github.com/users/${inputtwo.value}/repos`)
     .then((response) => response.json())
     .then((repositories) => {
-      reponumbertwo.innerHTML = `Number Of Repos : ${repositories.length}`;
+      reponumbertwo.innerHTML = `Number Of Repos: ${repositories.length}`;
     });
+});
+
+let button = document.querySelector(".button");
+button.addEventListener("click", function () {
+  if (inputtwo.value !== "" && input.value !== "") {
+    if (parseInt(reponumbertwo.innerHTML.split(" ")[3]) > parseInt(reponumber.innerHTML.split(" ")[3])) {
+      alert(inputtwo.value + " is the winner");
+    } else {
+      alert(input.value + " is the winner");
+    }
+  } else {
+    alert("Please enter a value");
+  }
 });
