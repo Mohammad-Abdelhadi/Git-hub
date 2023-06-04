7ad8148073801105eed9f5b9c0f0002eccb68d63
unknown <Mohammad-Abdelhadi>
2023-06-04 03:01:47 +0300

    Edit The Celbreate Function , and add background comparison page

--- js/comparison.js
+++ js/comparison.js
@@ -1,5 +1,4 @@
-let form = document.querySelector(".form");
-let input = document.querySelector(".input");
+let input = document.querySelector(".form .input");
 let username = document.querySelector(".username");
 let img = document.querySelector(".leftside img");
 let reponumber = document.querySelector(".reponumber");
@@ -10,12 +9,11 @@ input.addEventListener("input", function () {
     .then((data) => {
       username.innerHTML = data.name || data.login;
       img.src = data.avatar_url;
-      reponumber.innerHTML = `Number Of Repos: ${data.public_repos}`;
+      reponumber.innerHTML = `Repositories: ${data.public_repos}`;
     });
 });
 
-let formtwo = document.querySelector(".formtwo");
-let inputtwo = document.querySelector(".inputtwo");
+let inputtwo = document.querySelector(".formtwo .inputtwo");
 let usernametwo = document.querySelector(".usernametwo");
 let imgtwo = document.querySelector(".leftside .imgtwo");
 let reponumbertwo = document.querySelector(".reponumbertwo");
@@ -26,54 +24,45 @@ inputtwo.addEventListener("input", function () {
     .then((data) => {
       usernametwo.innerHTML = data.name || data.login;
       imgtwo.src = data.avatar_url;
-      reponumbertwo.innerHTML = `Number Of Repos: ${data.public_repos}`;
+      reponumbertwo.innerHTML = `Repositories: ${data.public_repos}`;
     });
 });
+
+// Add the following function to repeat the confetti effect multiple times
+function repeatConfetti() {
+  for (let i = 0; i < 15; i++) {
+    confetti();
+  }
+}
 
 let button = document.querySelector(".button");
 button.addEventListener("click", function () {
   if (inputtwo.value !== "" && input.value !== "") {
-    if (
-      parseInt(reponumbertwo.innerHTML.split(" ")[3]) >
-      parseInt(reponumber.innerHTML.split(" ")[3])
-    ) {
-      // Add celebration animation to winner
-      const start = () => {
-        setTimeout(function () {
-          for (let i = 0; i < 6; i++) {
-            confetti.start();
-          }
-        }, 100);
-      };
+    const repoCount1 = parseInt(reponumber.innerHTML.split(":")[1]);
+    const repoCount2 = parseInt(reponumbertwo.innerHTML.split(":")[1]);
 
-      start();
+    if (repoCount2 > repoCount1) {
+      Swal.fire({
+        title: `${usernametwo.innerHTML} <span style="color: green;">is the winner!</span>`,
+        text: "Congratulations!",
+        icon: "success",
+      });
 
-      // Show confetti
-      for (let i = 0; i < 6; i++) {
-        confetti();
-      }
+      repeatConfetti(); // Repeat the confetti effect
+    } else if (repoCount1 > repoCount2) {
+      Swal.fire({
+        title: `${username.innerHTML} <span style="color: green;">is the winner!</span>`,
+        text: "Congratulations!",
+        icon: "success",
+      });
 
-      Swal.fire(inputtwo.value + " Is winner", "Congratulations!", "success");
+      repeatConfetti(); // Repeat the confetti effect
     } else {
-      // Hide loser
-      // Add celebration animation to winner
-
-      Swal.fire(input.value + " Is winner", "Congratulations!", "success");
-
-      const start = () => {
-        setTimeout(function () {
-          for (let i = 0; i < 6; i++) {
-            confetti.start();
-          }
-        }, 100);
-      };
-
-      start();
-
-      // Show confetti
-      for (let i = 0; i < 6; i++) {
-        confetti();
-      }
+      Swal.fire(
+        "It's a tie!",
+        "Both users have the same number of repositories.",
+        "info"
+      );
     }
   } else {
     Swal.fire({
